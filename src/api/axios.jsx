a650031716eced2b1c8155f1913ec80b81b075b5
Dinesh13794 <13794dinesh@gmail.com>
2025-02-19 09:44:50 +0530

    renamed url

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://hello-8d2r.onrender.com/api',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`
   }
